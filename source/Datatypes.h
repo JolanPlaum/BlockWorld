4f96164d3dbcb61b21ac2fb1cb4cf0d9dd112cf6
Jolan Plaum <jolan.plaum@student.howest.be>
2023-02-01 21:40:56 +0100

    Added enums

--- source/Datatypes.h
+++ source/Datatypes.h
@@ -7,4 +7,21 @@ namespace dae
 		Vector3 position{};
 		Vector2 uv{};
 	};
+
+	enum class BlockType : BYTE
+	{
+		air,
+
+		water,
+		dirt,
+		sand,
+		stone,
+	};
+
+	enum class FaceDirection : BYTE
+	{
+		front, back,
+		bottom, top,
+		left, right,
+	};
 }
