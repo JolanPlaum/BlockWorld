a404eff2147a5f923081d925db28d17d8df9eb66
Jolan Plaum <jolan.plaum@student.howest.be>
2023-02-03 16:50:04 +0100

    Fixed neighbor chunk faces

--- source/Chunk.cpp
+++ source/Chunk.cpp
@@ -6,6 +6,7 @@
 #include "Mesh.h"
 #include "Material.h"
 #include "Atlas.h"
+#include "ChunkManager.h"
 
 using namespace dae;
 
@@ -34,9 +35,48 @@ Mesh* Chunk::Initialize(ID3D11Device* pDevice, Material* pMaterial, Coord coord)
 	std::vector<Vertex> vertices{};
 	vertices.reserve(width * width * heigth);
 
+	//Add side faces
+	Chunk* front = ChunkManager::GetInstance().GetChunk(coord.x, coord.z - 1);
+	Chunk* back = ChunkManager::GetInstance().GetChunk(coord.x, coord.z + 1);
+	Chunk* left = ChunkManager::GetInstance().GetChunk(coord.x - 1, coord.z);
+	Chunk* right = ChunkManager::GetInstance().GetChunk(coord.x + 1, coord.z);
+
 	coord.x *= width;
 	coord.z *= width;
 
+	for (int y{}; y < Chunk::heigth; ++y)
+	{
+		//front/back
+		for (int x{}; x < Chunk::width; ++x)
+		{
+			if (blocks[x][y][0] != BlockType::air && front != nullptr && front->blocks[x][y][width - 1] == BlockType::air)
+			{
+				AddFaceFront(vertices, x, y, 0, coord);
+			}
+
+			if (blocks[x][y][width - 1] != BlockType::air && back != nullptr && back->blocks[x][y][0] == BlockType::air)
+			{
+				AddFaceBack(vertices, x, y, width - 1, coord);
+			}
+		}
+
+		//left/right
+		for (int z{}; z < Chunk::width; ++z)
+		{
+			if (blocks[0][y][z] != BlockType::air && left != nullptr && left->blocks[width - 1][y][z] == BlockType::air)
+			{
+				AddFaceLeft(vertices, 0, y, z, coord);
+			}
+
+			if (blocks[width - 1][y][z] != BlockType::air && right != nullptr && right->blocks[0][y][z] == BlockType::air)
+			{
+				AddFaceRight(vertices, width - 1, y, z, coord);
+			}
+		}
+	}
+
+
+	//Add center faces
 	for (int x{}; x < Chunk::width; ++x)
 	{
 		for (int z{}; z < Chunk::width; ++z)
@@ -45,13 +85,13 @@ Mesh* Chunk::Initialize(ID3D11Device* pDevice, Material* pMaterial, Coord coord)
 			{
 				if (blocks[x][y][z] == BlockType::air) continue;
 
-				//front
-				if (z + 1 < width && blocks[x][y][z + 1] == BlockType::air)
-					AddFaceFront(vertices, x, y, z, coord);
-
 				//back
-				if (z > 0 && blocks[x][y][z - 1] == BlockType::air)
+				if (z + 1 < width && blocks[x][y][z + 1] == BlockType::air)
 					AddFaceBack(vertices, x, y, z, coord);
+
+				//front
+				if (z > 0 && blocks[x][y][z - 1] == BlockType::air)
+					AddFaceFront(vertices, x, y, z, coord);
 
 				//top
 				if (y + 1 < heigth && blocks[x][y + 1][z] == BlockType::air)
@@ -103,10 +143,10 @@ void Chunk::AddFaceFront(std::vector<Vertex>& vector, int x, int y, int z, const
 void Chunk::AddFaceBack(std::vector<Vertex>& vector, int x, int y, int z, const Coord& c)
 {
 	std::vector<Vector2> temp = Atlas::GetUVs(blocks[x][y][z], FaceType::back);
-	vector.push_back({ {c.x + x + 1.f, y + 1.f, c.z + z + 0.f}, temp[0] });
-	vector.push_back({ {c.x + x + 1.f, y + 0.f, c.z + z + 0.f}, temp[1] });
-	vector.push_back({ {c.x + x + 0.f, y + 1.f, c.z + z + 0.f}, temp[2] });
-	vector.push_back({ {c.x + x + 0.f, y + 0.f, c.z + z + 0.f}, temp[3] });
+	vector.push_back({ {c.x + x + 0.f, y + 1.f, c.z + z + 1.f}, temp[0] });
+	vector.push_back({ {c.x + x + 0.f, y + 0.f, c.z + z + 1.f}, temp[1] });
+	vector.push_back({ {c.x + x + 1.f, y + 1.f, c.z + z + 1.f}, temp[2] });
+	vector.push_back({ {c.x + x + 1.f, y + 0.f, c.z + z + 1.f}, temp[3] });
 }
 
 void Chunk::AddFaceBottom(std::vector<Vertex>& vector, int x, int y, int z, const Coord& c)
