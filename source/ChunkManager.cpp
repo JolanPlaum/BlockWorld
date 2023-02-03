02f521cd6c71a461258880652e63e627af439b37
Jolan Plaum <jolan.plaum@student.howest.be>
2023-02-03 15:19:59 +0100

    Create ChunkManager class

---
+++ source/ChunkManager.cpp
@@ -0,0 +1,70 @@
+//-----------------------------------------------------------------
+// Includes
+//-----------------------------------------------------------------
+#include "pch.h"
+#include "ChunkManager.h"
+
+using namespace dae;
+
+
+//-----------------------------------------------------------------
+// Constructors
+//-----------------------------------------------------------------
+ChunkManager::ChunkManager()
+{
+	for (int i{}; i < m_WorldSize; ++i)
+	{
+		Coord coord{ i % m_WorldWidth, i / m_WorldWidth };
+		m_ChunkCoords[i] = coord;
+		LoadChunk(m_Chunks[i]);
+	}
+}
+
+
+//-----------------------------------------------------------------
+// Destructor
+//-----------------------------------------------------------------
+ChunkManager::~ChunkManager()
+{
+}
+
+
+//-----------------------------------------------------------------
+// Public Member Functions
+//-----------------------------------------------------------------
+Chunk& ChunkManager::GetChunk(int x, int z)
+{
+	auto it = std::find(m_ChunkCoords.begin(), m_ChunkCoords.end(), Coord{x, z});
+
+	int index = static_cast<int>(it - m_ChunkCoords.begin());
+	return m_Chunks[index % m_WorldSize];
+}
+
+
+//-----------------------------------------------------------------
+// Private Member Functions
+//-----------------------------------------------------------------
+void ChunkManager::LoadChunk(Chunk& chunk)
+{
+	for (int x{}; x < Chunk::width; ++x)
+	{
+		for (int z{}; z < Chunk::width; ++z)
+		{
+			for (int y{}; y < Chunk::heigth; ++y)
+			{
+				chunk.blocks[x][y][z] = GetBlockType(x, y, z);
+			}
+		}
+	}
+}
+
+BlockType ChunkManager::GetBlockType(int x, int y, int z)
+{
+	int waterHeight{ 10 }, groundHeight{ 3 };
+	int dirtHeight = std::max(x, z) + groundHeight;
+
+	if (y < dirtHeight) return BlockType::dirt;
+	if (y < waterHeight) return BlockType::water;
+	return BlockType::air;
+}
+
---
+++ source/ChunkManager.h
@@ -0,0 +1,44 @@
+#pragma once
+// Includes
+#include "Chunk.h"
+
+namespace dae
+{
+	// Class Forward Declarations
+	
+	// Class Declaration
+	class ChunkManager final
+	{
+	public:
+		// Constructors and Destructor
+		explicit ChunkManager();
+		~ChunkManager();
+		
+		// Copy and Move semantics
+		ChunkManager(const ChunkManager& other)					= delete;
+		ChunkManager& operator=(const ChunkManager& other)		= delete;
+		ChunkManager(ChunkManager&& other) noexcept				= delete;
+		ChunkManager& operator=(ChunkManager&& other) noexcept	= delete;
+	
+		//---------------------------
+		// Public Member Functions
+		//---------------------------
+		static const int m_WorldWidth{ 2 };
+		static const int m_WorldSize{ m_WorldWidth * m_WorldWidth };
+
+		Chunk& GetChunk(int x, int z);
+	
+	
+	private:
+		// Member variables
+		Chunk m_Chunks[m_WorldSize]{};
+		std::vector<Coord> m_ChunkCoords{ m_WorldSize };
+	
+		//---------------------------
+		// Private Member Functions
+		//---------------------------
+		void LoadChunk(Chunk& chunk);
+		BlockType GetBlockType(int x, int y, int z);
+	};
+}
+
